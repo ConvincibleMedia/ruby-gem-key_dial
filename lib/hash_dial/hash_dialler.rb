a294b6b07710c1e7e90a15f046f3ed54f099f978
Virgil Ierubino <webmaster@convincible.media>
2019-01-08 18:33:53 +0000

    Added documentation

--- lib/hash_dial/hash_dialler.rb
+++ lib/hash_dial/hash_dialler.rb
@@ -18,12 +18,20 @@ module HashDial
 			end
 		end
 
+		# Adds a hash key to the list of nested keys to try, one level deeper.
+		#
+		# @param keys The key to add. Multiple arguments would add multiple keys.
+		#
 		def dial!(*keys)
 			#unless key.is_a(Symbol) || key.is_a(String)
 			@lookup += keys
 			return self
 		end
 
+		# Digs into the hash to the list of keys specified by dialling. Returns nil or default if specified.
+		#
+		# @param default What to return if no key is found.
+		#
 		def call(default = nil)
 			begin
 				value = @hash.dig(*@lookup)
@@ -33,10 +41,15 @@ module HashDial
 			return value
 		end
 
+		# Return the original hash object.
 		def hangup
 			return @hash
 		end
 
+		# Remove keys from the dialling list.
+		#
+		# @param keys If specified, these keys would be removed from wherever they appear in the dialling list. Otherwise, the last added key is removed.
+		#
 		def undial!(*keys)
 			if keys.length > 0
 				@lookup -= keys
@@ -46,6 +59,8 @@ module HashDial
 			return self
 		end
 
+		# The preferred way to build up your dialling list. Access HashDialler as if it were a Hash, e.g. hash[a][b][c]. This does not actually return any value, rather it dials those keys (awaiting a call).
+		#
 		def [](key)
 			return dial!(key)
 		end
