d9c67f49849d6579e6e5517945f72a9ad6e3d279
Virgil Ierubino <development@convincible.media>
2019-01-28 00:35:24 +0000

    New dial = (setting) capability (in progress)

--- lib/key_dial/key_dialler.rb
+++ lib/key_dial/key_dialler.rb
@@ -82,6 +82,134 @@ module KeyDial
 			return dial!(key)
 		end
 
+		# Set any deep key. If keys along the way don't exist, empty Hashes or Arrays will be created. Warning: this method will try to coerce your main object to match the structure implied by your keys.
+		def []=(key_obj, value_obj)
+			# Hashes can be accessed at [Object] of any kind
+			# Structs can be accessed at [String] and [Symbol], and [Integer] for the nth member (or [Float] which rounds down)
+			# Arrays can be accessed at [Integer] or [Float] which rounds down
+
+			# Dial the key to be set - @lookup can never be empty
+			dial!(key_obj)
+
+			index = 0
+			@lookup.inject(@obj_with_keys) { |deep_obj, this_key|
+
+				# this = object to be accessed
+				# key = key to access on this
+				# access = what kind of key is key
+
+				next_key = @lookup[index + 1]
+				last_key = @lookup[index - 1] if index > 0
+				key = {
+					this: {
+						type: nil,
+						value: this_key
+					},
+					next: {
+						type: nil,
+						value: next_key
+					}
+				}
+
+				[:this, :next].each { |which|
+					if key[which][:value].is_a?(Integer) || key[which][:value].is_a?(Float)
+						key[which][:type] = :number
+						key[which][:max] = key[which][:value].magnitude.floor + 1
+					else
+						key[which][:type] = :object
+						key[which][:type] = :string if key[which][:value].is_a?(String)
+						key[which][:type] = :symbol if key[which][:value].is_a?(Symbol)
+					end
+				}
+
+				# Ensure this object is a supported type - always true for index == 0
+				if !deep_obj.class.included_modules.include?(KeyDial)
+					if key[:this][:type] == :number
+						puts 'woop'
+						deep_obj = Array.new(key[:this][:max] - 1).unshift(deep_obj)
+					else
+						deep_obj = {0 => deep_obj}
+					end
+					# Go back and reinject this altered value into the array
+					@lookup[0...(index-1)].inject(@obj_with_keys) { |deep_obj2, this_key2|
+						deep_obj2[this_key2]
+					}[last_key] = deep_obj
+				end
+
+				# Does this object already have this key?
+				if !deep_obj.call(key[:this][:value])
+					# If not, create empty array/hash dependant on upcoming key
+					if key[:next][:type] == :number
+						deep_obj[key[:this][:value]] = Array.new(key[:next][:max])
+					else
+						deep_obj[key[:this][:value]] = {key[:next][:value] => nil}
+					end
+				end#else
+					# Such key was found
+					#if deep_obj[key[:this][:value]].is_a?
+
+
+				# Quit if this is the penultimate or last iteration
+				next deep_obj if index >= @lookup.size - 1
+
+				# Increment index manually
+				index += 1
+
+				# Before here, we must make sure we can access key on deep_obj
+				deep_obj[key[:this][:value]]
+
+			# Final access (and set) of last key in the @lookup
+			}[@lookup[-1]] = value_obj
+
+
+			if false
+
+				# WHAT TO DO WHEN SETTING A KEY YOU JUST CAN'T SET
+
+				# "You asked for it!"(TM)
+				# If accessing a member that doesn't exist, we'll replace the struct with a redefined anonymous one containing the members you wanted. This is dangerous but it's your fault.
+				if this.is_a?(Struct)
+					if access == :string || access == :symbol
+						if !this.members.include?(key.to_sym)
+							# You asked for it!
+							# Add the member you requested
+							new_members = this.members.push(key.to_sym)
+							this = Struct.new(*new_members).new(*this.values)
+						end
+					elsif access == :number
+						if max > this.size
+							# You asked for it!
+							# Create new numeric members up to key requested
+							new_members = this.members.concat(
+								(this.size..max).to_a.map { |num| num.to_sym }
+							)
+							this = Struct.new(*new_members).new(*this.values)
+						end
+					end
+
+
+				end
+
+				# "You asked for it!"(TM)
+				# If accessing an array with a key that doesn't exist, we'll add elements to the array or change the array to a hash. This is dangerous but it's your fault.
+				if this.is_a?(Array)
+					if access == :number
+						if key <= -1 && max > this.size
+							# You asked for it!
+							# The only time an Array will break is if you try to set a negative key larger than the size of the array. In this case we'll prepend your array with nils.
+							this = Array.new(max - this.size, nil).concat(this)
+						end
+					else
+						# You asked for it!
+						# Trying to access non-numeric key on an array, so will convert the array into a hash with integer keys.
+						this = this.each_with_index.map { |v, index| [index, v] }.to_h
+					end
+				end
+
+			end
+
+		end
+
 		# You can add and subtract keys from a KeyDialler object
 		def +(key)
 			return dial!(key)
