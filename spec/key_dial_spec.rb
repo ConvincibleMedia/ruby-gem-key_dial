cae9acd25a9550fdd6d2e0909876542fab901b48
Virgil Ierubino <development@convincible.media>
2019-01-29 00:21:48 +0000

    New tests

--- spec/key_dial_spec.rb
+++ spec/key_dial_spec.rb
@@ -3,20 +3,24 @@ RSpec.describe KeyDial do
         expect(KeyDial::VERSION).not_to be nil
     end
 
-    test_original = {a: {b: {c: true}, d: 5}, e: [0, 1]}.freeze
-    test = test_original.dup
+    test_original = {a: {b: {c: true}, d: 5}, e: [0, 1], f: Struct.new(:g).new('hello')}.deep_freeze
+    test = test_original.deep_dup
 
-    # Shorthand i.e. hash.dial[a][b]
+    # Shorthand e.g. hash.dial[a][b]
 
     it "can be used in shorthand and digs out the correct value" do
         expect(test.dial[:a][:b][:c].call).to eq(true)
+        expect(test.dial[:e][1].call).to eq(1)
+        expect(test.dial[:f][:g].call).to eq('hello')
     end
 
     it "can be used in shorthand and returns nil for a nonexistent key" do
         expect(test.dial[:a][:x][:c].call).to eq(nil)
+        expect(test.dial[:e][:x][:c].call).to eq(nil)
+        expect(test.dial[:f][:x][:c].call).to eq(nil)
     end
 
-    it "can be used in shorthand and returns nil if requesting a key on a non-hash" do
+    it "can be used in shorthand and returns nil if requesting a key on a non-keyed object" do
         expect(test.dial[:a][:d][:c].call).to eq(nil)
     end
 
@@ -24,7 +28,7 @@ RSpec.describe KeyDial do
         expect(test).to eq(test_original)
     end
 
-    # Dig i.e. (a, b)
+    # Dig e.g. hash.dial(a, b)
 
     it "can be used like dig() and digs out the correct value" do
         expect(test.dial(:a, :b, :c).call).to eq(true)
@@ -128,6 +132,66 @@ RSpec.describe KeyDial do
     it "can't access KeyDialler.use_keys" do
         x = KeyDial::KeyDialler.new
         expect{x.use_keys(:a, :b, :c)}.to raise_error(NoMethodError)
+    end
+
+    # Setting
+
+    it "can set a value on a dialled deep hash key that exists" do
+        test.dial[:a][:b][:c] = 'test'
+        expect(test[:a][:b][:c]).to eq('test')
+    end
+
+    it "can add a value at a dialled deep hash key that does not exist" do
+        test.dial[:a][:b][:f] = 'test'
+        expect(test[:a][:b][:f]).to eq('test')
+    end
+
+    it "can set a value on a dialled deep array key that exists" do
+        test.dial[:e][0] = 'test'
+        expect(test[:e][0]).to eq('test')
+    end
+
+    it "can add a value at a dialled deep array key that does not exist" do
+        test.dial[:e][2] = 'test'
+        expect(test[:e][2]).to eq('test')
+    end
+
+    it "can add a value at a negative array key that does not exist" do
+        test.dial[:e][-5] = 'test'
+        expect(test[:e][-5]).to eq('test')
+    end
+
+    it "can set a value on a dialled deep struct key that exists" do
+        test.dial[:f][:g] = 'test'
+        expect(test[:f][:g]).to eq('test')
+        test.dial[:f][0] = 'test2'
+        expect(test[:f][0]).to eq('test2')
+    end
+
+    it "can set a value on a negative struct key that does not exist" do
+        test.dial[:f][-4] = 'test3'
+        expect(test[:f][-4]).to eq('test3')
+    end
+
+    it "can add a value at a dialled deep struct key that does not exist" do
+        test.dial[:f][:h] = 'test'
+        expect(test[:f][:h]).to eq('test')
+        test.dial[:f][1] = 'test2'
+        expect(test[:f][1]).to eq('test2')
+    end
+
+    test = test_original.deep_dup
+
+    it "can change a deep key from array to hash if required" do
+        test.dial[:e]['string'] = 'test'
+        expect(test.dial[:f]['string'].call('test')).to eq('test')
+    end
+
+    it "can create missing hashes or arrays along the way" do
+        test.dial[:a][3] = 'test'
+        expect(test[:a][3]).to eq('test')
+        test.dial[:a][2][:z] = 'test'
+        expect(test[:a][2][:z]).to eq('test')
     end
 
 end
