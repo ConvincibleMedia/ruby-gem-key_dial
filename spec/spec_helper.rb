f258862f26cf46f25a11d584d0e65249fa02763b
Convincible <development@convincible.media>
2019-09-25 11:50:06 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require "active_support/core_ext/object/deep_dup"
 
 RSpec.configure do |config|
 	# Enable flags like --only-failures and --next-failure
-	config.example_status_persistence_file_path = ".rspec_status"
+	#config.example_status_persistence_file_path = ".rspec_status"
 
 	# Disable RSpec exposing methods globally on `Module` and `main`
 	#config.disable_monkey_patching!
